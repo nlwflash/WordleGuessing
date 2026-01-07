fb6ed449cea58f248e9693ae627a93dea855f0b3
Nicholas Weigand <nlw1210@gmail.com>
2026-01-06 23:02:07 -0500

    Removing type error warning from extension settings on this file

--- source_code/utility/helper/default_dict_set.py
+++ source_code/utility/helper/default_dict_set.py
@@ -5,5 +5,5 @@ K = TypeVar("K")
 V = TypeVar("V")
 
 class DefaultDictSet(DefaultDict[K, set[V]], Generic[K, V]):
-    def __init__(self, factory=set):
+    def __init__(self, factory=set): # type: ignore
         super().__init__(factory)
