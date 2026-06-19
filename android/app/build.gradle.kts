41f909d7d4bf6d956ab2ae83701091e2d8f90a61
Nicholas Weigand <nlw1210@gmail.com>
2026-06-18 23:26:10 -0400

    build: enable local android sdk builds and wrapper

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -107,6 +107,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.google.material)
 
     testImplementation(libs.junit4)
 
--- android/app/src/main/kotlin/com/nickwe/wordleguessing/android/ui/WordleGuessingScreen.kt
+++ android/app/src/main/kotlin/com/nickwe/wordleguessing/android/ui/WordleGuessingScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -28,7 +29,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.nickwe.wordleguessing.android.solver.SolverUiState
